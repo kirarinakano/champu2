b4da376b9d017a2d8fdc7f24fd52ab937cc79e0f
kirarinakano <kirari624@gmail.com>
2018-06-19 11:11:30 +0900

    add .DS_Store 1.php callback.php config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+require 'autoload.php';
+define('CONSUMER_KEY', 'xc6qkyKKqKqEfGekvHlflAp1s');
+define('CONSUMER_SECRET', 'cYNKe6BRLkQiJ5KSb3DPfV1LPJTssV0W2imWXFuoH7vhX4FLnf'); 												
+define('OAUTH_CALLBACK', 'http://localhost/champu2/callback.php');
